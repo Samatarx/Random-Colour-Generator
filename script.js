e5f5f987e0d946790c4323f48996f4b0cc391412
Samatar Xasan <samatar.xasan@gmail.com>
2020-09-11 00:08:34 +0100

    Js comments

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const background = document.body.style
-const textColor = document.querySelector(".hexcode").style
-const button = document.querySelector(".btn")
-const text = document.querySelector("span")
-const letters = document.querySelector("letter")
+const background = document.body.style;
+const textColor = document.querySelector(".hexcode").style;
+const button = document.querySelector(".btn");
+const text = document.querySelector("span");
+const letters = document.querySelector("letter");
 
-button.addEventListener ('click', function(){
-    let hexcol= "#";
-for (var i = 0; i < 6; i++) {
-  hexcol += hex[getRandom()];
-}
-
-background.backgroundColor = hexcol;
-textColor.color = hexcol;
-text.innerHTML = hexcol;
-
-document.querySelector("textarea").innerHTML = `background-color: ${hexcol};`
-
-})
-
-
+//Selects a random number from the hex array defined above
 function getRandom() {
   return Math.floor(Math.random() * hex.length);
 }
+
+//This function creates a randomly generated colour once the button is pressed
+button.addEventListener("click", function () {
+  let hexcol = "#";
+  //Returns a random value from the getRandom() function and then combines 6 values with the # to form a hex code
+  for (var i = 0; i < 6; i++) {
+    hexcol += hex[getRandom()];
+  }
+
+  //Changes the background color to match the randomly generated colours
+  background.backgroundColor = hexcol;
+  textColor.color = hexcol;
+  text.innerHTML = hexcol;
+
+  // This area changes the text area and provides the CSS properties in a easy to copy format
+  document.querySelector("textarea").innerHTML = `background-color: ${hexcol};`;
+});
 
 //Button copies the textarea into the clipboard
 const copytextarea = document.querySelector(".copybtn");
